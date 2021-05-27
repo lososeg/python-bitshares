9e051682318873cb14a3aa9e42302d475b921e87
lososeg <ya.lososeg@gmail.com>
2021-05-28 02:06:31 +0300

    Update chain_id

--- bitsharesbase/chains.py
+++ bitsharesbase/chains.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 known_chains = {
-    "BTS": {
-        "chain_id": "4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8",
-        "core_symbol": "BTS",
-        "prefix": "BTS",
-    },
-    "TEST": {
-        "chain_id": "39f5e2ede1f8bc1a3a54a7914414e3779e33193f1f5693510e73cb7a87617447",
-        "core_symbol": "TEST",
-        "prefix": "TEST",
+    "NBS": {
+        "chain_id": "cd931cb96d657ff0ef0226f7ae9d25175b3cc96a84490a674ed36170830324e7",
+        "core_symbol": "NBS",
+        "prefix": "NBS",
     },
 }
