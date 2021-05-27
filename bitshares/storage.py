edc4170cbdd0bbe174a4378c9053ac5f25a3981a
lososeg <ya.lososeg@gmail.com>
2021-05-28 02:05:38 +0300

    Update node url

--- bitshares/storage.py
+++ bitshares/storage.py
@@ -10,7 +10,7 @@ from graphenestorage import (
 )
 
 
-url = "wss://node.bitshares.eu"
+url = "wss://api-nbs-northchina.cnvote.vip:80"
 SqliteConfigurationStore.setdefault("node", url)
 SqliteConfigurationStore.setdefault("order-expiration", 356 * 24 * 60 * 60)
 
